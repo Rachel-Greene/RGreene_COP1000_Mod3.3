a7612dee35b7395bc647472dcbc1fce03e3b703d
Rachel-Greene <159064114+Rachel-Greene@users.noreply.github.com>
2024-03-06 14:52:13 +0000

    added in if/else statements for day validation

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 # This code determines if a date entered is valid #
 
-## ----- Housekeeping() ----- ##
+## ---------- Housekeeping() ---------- ##
 # Variables #
 validDate = True
-MIN_YEAR = 0
-MIN_MONTH = 1
-MAX_MONTH = 12
-MIN_DAY = 1
-MAX_DAY = 31
+validDay = True
+minYear = 0
+minMonth = 1
+maxMonth = 12
+minDay = 1
+maxDay30 = 30
+maxDay31 = 31
+maxDay29 = 29
 
 # Input Statements #
-print("Please enter numeric values below.")
+print("Please enter numeric values of the date you wish to validate.")
 month = input("Month: ")
 day = input("Day: ")
 year = input("Year: ")
-
-# Convert Inputs #
-day = int(day)
+# convert inputs #
 month = int(month)
+day = int(day)
 year = int(year)
 
 
-## ----- Detail() ----- ##
-if (year) <= MIN_YEAR:
-    validDate = False
-elif (month) < MIN_MONTH or (month) > MAX_MONTH:
-    validDate = False
-elif (day) < MIN_DAY or (day) > MAX_DAY:
-    validDate = False
+## ---------- Detail() ---------- ##
+# Check Validity #
+if month in [1, 3, 5, 7, 8, 10, 12]:
+  validDay = day >= minDay and day <= maxDay31
+elif month in [4, 6, 9, 11]:
+  validDay = day >= minDay and day <= maxDay30
+elif month in [2]:
+  validDay = day >= minDay and day <= maxDay29
+else: validDay = False
+
+if year <= minYear:
+  validDate = False
+elif month < minMonth and month > maxMonth:
+  validDate = False
+elif validDay == False:
+  validDate = False
 
 
-## ----- endOfJob() ----- ##
+## ---------- endOfJob() ---------- ##
 if validDate == True:
     print(f"{month}/{day}/{year} is a valid date")
 else:
